10f43fa0fb31bed7913026fff025098db1da8a14
Farshad G. Veshki <35424873+FarshadGVeshki@users.noreply.github.com>
2022-03-29 16:15:01 +0300

    Update hist_update.m

--- utility/hist_update.m
+++ utility/hist_update.m
@@ -5,15 +5,17 @@ function [H,invMtx] = hist_update(H_old,invMtx_old,x,s,sig,t)
 % Y. Wang, Q. Yao, J. T. Kwok and L. M. Ni, "Scalable Online Convolutional Sparse Coding," 
 % in IEEE Transactions on Image Processing, vol. 27, no. 10, pp. 4850-4859, Oct. 2018, doi: 10.1109/TIP.2018.2842152.
 
-
+[h,w,K] = size(x);
+if isempty(H_old)
+    invMtx_old = zeros(h,w,K,K);
+    H_old = zeros(h,w,K);
+end
 xf = fft2(x);
-[h, w] = size(s);
-K = size(x,3);
 P = h*w;
 H = (1-1/t)*H_old + (1/t)*conj(xf).*fft2(s);
 if t == 1
-   invMtx = (repmat(reshape(eye(K),[1 1 K K]),[h w,1,1])-((conj(xf).*reshape(xf,[h w 1 K]))./(sig*P+sum(conj(xf).*xf,3))))/(sig*P);
+   invMtx = (repmat(reshape(eye(K),[1 1 K K]),[h w,1,1])-(((xf).*reshape(conj(xf),[h w 1 K]))./(sig*P+sum(conj(xf).*xf,3))))/(sig*P);
 else
-   invMtx = (t/(t-1))*(invMtx_old-(sum(invMtx_old.*reshape(xf,[h,w,1,K]),4).*sum(conj(xf).*invMtx_old,3))./(t-1+ sum(sum(conj(xf).*invMtx_old,3).*reshape(xf,[h,w,1,K]),4)));
+   invMtx = (t/(t-1))*(invMtx_old-(sum(invMtx_old.*reshape(xf,[h,w,1,K]),4).*sum(conj(xf).*invMtx_old,3))./(t-1+ reshape(sum(sum(conj(xf).*invMtx_old,3).*reshape(xf,[h,w,1,K]),4),[h w])));
 end
 end
